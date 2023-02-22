00b8754e76e4015835b201558e3c19a21a9dc9ab
Angelo Mendoza <angelo@modiphy.net>
2023-02-22 13:32:37 +0800

    Install next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+
+const config = {
+  siteUrl: "https://www.modiphy.com/", // Placeholder URL. Change to url of the site.
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  generateIndexSitemap: false,
+};
+
+module.exports = config;
