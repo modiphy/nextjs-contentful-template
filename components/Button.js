0857c2dddddac334ca15dc60b11c70e8d11318fe
Angelo Mendoza <angelo@modiphy.net>
2023-02-22 23:52:25 +0800

    Fix Link href in Button component

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Button({ text, pageRef, url, className = "" }) {
   return (
     <Link
-      href={pageRef ? `/${pageRef.fields.slug}` : url}
+      href={`${pageRef ? `/${pageRef.fields.slug}` : url}`}
       className={className}
     >
       {text}
