11841ebf8478aba94cac724c2d8d0e2e80fa2369
Angelo Mendoza <angelo@modiphy.net>
2023-03-31 23:03:13 +0800

    Fix layout component

--- app/layout.js
+++ app/layout.js
@@ -3,14 +3,24 @@ import "../styles/blend.css";
 import "../styles/colors.css";
 import "../styles/header.css";
 import Header from "@/components/Header";
-import { getHeaderData } from "@/lib/api";
+import { getHeaderData, getMetaData } from "@/lib/api";
+
+export async function metadata({ params }) {
+  const metaData = await getMetaData();
+
+  return {
+    title: {
+      default: metaData?.fields?.title,
+      template: "%s | " + metaData?.defaultMetaTitle,
+    },
+  };
+}
 
 export default async function RootLayout({ children }) {
   const headerData = await getHeaderData();
 
   return (
-    <html>
-      <head />
+    <html lang="en">
       <body>
         <Header data={headerData} />
         <main>{children}</main>
