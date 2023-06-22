b08e33a67d4b7b3fdad323522161cee71833e2d8
Angelo Mendoza <angelo@modiphy.net>
2023-06-22 22:37:38 +0800

    Update contact info items

--- components/ContactInformationItem.js
+++ components/ContactInformationItem.js
@@ -2,13 +2,24 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 import Link from "next/link";
 
-export default function ContactInformationItem({ title, body }) {
+export default function ContactInformationItem({ title, body, url }) {
   return (
-    <div className="flex flex-col text-gray-200">
-      <div className="mb-1 font-semibold uppercase tracking-wide text-white">
+    <div>
+      <p className="text-xl font-medium uppercase tracking-wide text-primary-400">
         {title}
-      </div>
-      {documentToReactComponents(body, options)}
+      </p>
+      {url ? (
+        <Link
+          href={`${url}`}
+          className="group mt-3 block cursor-pointer text-lg text-gray-300"
+        >
+          {documentToReactComponents(body, options)}
+        </Link>
+      ) : (
+        <div className="mt-3 flex justify-center text-lg text-gray-300 md:block">
+          {documentToReactComponents(body, options)}
+        </div>
+      )}
     </div>
   );
 }
@@ -26,13 +37,13 @@ const options = {
       );
     },
     [BLOCKS.PARAGRAPH]: (node, children) => {
-      return <p className="mt-1 text-lg">{children}</p>;
+      return <p className="mt-1 text-lg group-hover:underline">{children}</p>;
     },
     [INLINES.HYPERLINK]: (node, children) => {
       return (
         <span className="text-theme-body mt-1 text-base leading-tight">
           <Link
-            href={node?.data?.uri}
+            href={`${node?.data?.uri}`}
             target="_blank"
             className="hover:text-white hover:underline"
           >
@@ -41,5 +52,23 @@ const options = {
         </span>
       );
     },
+    [BLOCKS.TABLE]: (node, children) => (
+      <table>
+        <tbody className="flex flex-col space-y-1 text-base text-gray-300">
+          {children}
+        </tbody>
+      </table>
+    ),
+    [BLOCKS.TABLE_ROW]: (node, children) => (
+      <tr className="flex space-x-3">
+        <td className="inline-block w-9">
+          {node?.content?.[0]?.content?.[0]?.content?.[0]?.value}
+        </td>
+        <td>{node?.content?.[1]?.content?.[0]?.content?.[0]?.value}</td>
+      </tr>
+    ),
+    [BLOCKS.TABLE_CELL]: (node, children) => (
+      <td>{node?.content?.[0]?.content?.[0]?.value}</td>
+    ),
   },
 };
