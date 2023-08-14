21a72cbd342d1f09fe422bad905d745b4e789d01
Angelo Mendoza <angelo@modiphy.net>
2023-08-14 20:06:19 +0800

    Blacklist 404 page in sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -5,6 +5,19 @@ const config = {
   generateRobotsTxt: true,
   changefreq: "daily",
   generateIndexSitemap: false,
+  transform: async (config, path) => {
+    // Check if the path should be excluded
+    if (path === "/404") {
+      return null; // Return null to exclude the URL from the sitemap
+    }
+
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+    };
+  },
 };
 
 module.exports = config;
